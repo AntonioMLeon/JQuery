b8ac32f95704335fa66be59bdbc9ee1e9f9faf0c
AntonioLeon <antoniomleonjimenez@gmail.com>
2024-01-05 15:11:53 +0100

    commit

---
+++ js/magiaoscura.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    const urlJson = '../json/magiaoscura.json'; 
+    const sectionPociones = $('section'); 
+
+    $.getJSON(urlJson, function (data) {
+        const magiaoscura = data.magiaoscura; 
+
+        $.each(magiaoscura, function (index, item) {
+            const divProducto = $('<div>').addClass('producto');
+            const imagen = $('<img>').attr('src', item.imagen).attr('alt', item.nombre).addClass('img-producto img-' + index);
+            const nombre = $('<h3>').text(item.nombre);
+            const precio = $('<p>').text('$' + item.precio.toFixed(2));
+            const descripcion = $('<p>').text(item.descripcion);
+
+            divProducto.append(imagen, nombre, precio, descripcion);
+
+            sectionPociones.append(divProducto); 
+        });
+
+        
+        if (window.location.href.toLowerCase().includes('magiaoscura')) {
+            $('#myModal').modal('show');
+        }
+    });
+
+    const modalContent = 
+    
+            `<div class="modal fade" id="myModal" tabindex="-1">
+                <div class="modal-dialog">
+                    <div class="modal-content bg-dark text-light">
+                        <div class="modal-header">
+                            <h5 class="modal-title text-warning">Advertencia</h5>
+                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="text-warning">No nos hacemos responsables del mal uso de estos objetos.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Cerrar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    
+    $('body').append(modalContent);
+});
+
